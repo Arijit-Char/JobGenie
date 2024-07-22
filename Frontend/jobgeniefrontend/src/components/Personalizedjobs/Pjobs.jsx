23e2dd2e57f15d540b090b664cd2e85c7718694b
Arijit-Char <arijitchar24@gmail.com>
2024-07-22 20:39:42 +0530

    .

--- Frontend/jobgeniefrontend/src/components/Personalizedjobs/Pjobs.jsx
+++ Frontend/jobgeniefrontend/src/components/Personalizedjobs/Pjobs.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import Cards from "./Cards";
 import "./Pjobs.scss";
 import { useDispatch, useSelector } from "react-redux";
