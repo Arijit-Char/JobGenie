7c2f978a05657b05352a326bc847c5f5aaebfb8a
Arijit-Char <arijitchar24@gmail.com>
2024-07-23 01:22:08 +0530

    Fixing Cards

--- Frontend/jobgeniefrontend/src/components/Personalizedjobs/PersonalizedJobListings/PersonalizedJobListingscards.jsx
+++ Frontend/jobgeniefrontend/src/components/Personalizedjobs/PersonalizedJobListings/PersonalizedJobListingscards.jsx
@@ -8,40 +8,46 @@ import logo from "../../../Data/logo.png";
 import { useNavigate } from "react-router-dom";
 import { useDispatch } from "react-redux";
 import { GetJobDetails } from "../../../actions/user";
+import { motion } from "framer-motion";
 import "./PersonalizedJobListings.scss";
 
 export default function PersonalizedJobListingscards({ job }) {
   const dispatch = useDispatch();
   const navigate = useNavigate();
+
   return (
-    <div
+    <motion.div
+      className="card-container2"
+      whileHover={{ scale: 1.05 }}
+      whileTap={{ scale: 0.95 }}
       onClick={() => {
         dispatch(GetJobDetails(job));
         navigate("/jobdetails");
       }}
     >
-      <Card sx={{ maxWidth: 455 }}>
+      {console.log(job)}
+      <Card className="job-card" sx={{ maxWidth: 345 }}>
         <CardActionArea>
           <CardMedia
             className="employer-logo"
             component="img"
-            height="140"
+            height="100"
             image={job.employer_logo ? job.employer_logo : logo}
             alt="Employer Logo"
           />
           <CardContent>
-            <Typography gutterBottom variant="h5" component="div">
+            <Typography className="job-title" gutterBottom variant="h5" component="div">
               {job.employer_name}
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography className="job-location" variant="body2" color="text.secondary">
               {job.job_city}, {job.job_country} ({job.job_employment_type})
             </Typography>
-            <Typography variant="body2" color="text.secondary">
+            <Typography className="job-position" variant="body2" color="text.secondary">
               {job.job_title}
             </Typography>
           </CardContent>
         </CardActionArea>
       </Card>
-    </div>
+    </motion.div>
   );
 }
